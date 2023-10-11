8c810a9cbef24a22084e9176311146e21e1c871a
jephthah <jephthah.mbah@outlook.com>
2023-10-11 17:48:46 +0100

    fixed cors errors

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(cookieParser());
 // app.use(cors());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://exam-gpt.vercel.app"],
     credentials: true,
   })
 );
